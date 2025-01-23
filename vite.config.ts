cc14e9c4db157d3cbbe547560b68fd47d2259af5
danno77-glu <80995319+danno77-glu@users.noreply.github.com>
2025-01-23 16:23:03 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+    import react from '@vitejs/plugin-react';
 
     // https://vitejs.dev/config/
     export default defineConfig({
@@ -13,4 +13,4 @@ import { defineConfig } from 'vite'
           "Cross-Origin-Resource-Policy": "cross-origin"
         }
       }
-    })
+    });
